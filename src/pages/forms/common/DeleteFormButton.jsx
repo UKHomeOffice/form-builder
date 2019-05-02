a4c7d56953906ba51c6a390ad1eee7f38a7de130
Amin <amin@digitalpatterns.io>
2019-05-02 18:15:17 +0100

    fixing button when successful

--- src/pages/forms/common/DeleteFormButton.jsx
+++ src/pages/forms/common/DeleteFormButton.jsx
@@ -1,13 +1,23 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import {Button, Confirm, Container, Icon, Message} from "semantic-ui-react";
 import useApiRequest from "../../../core/api";
-import {ERROR, EXECUTING} from "../../../core/api/actionTypes";
+import {ERROR, EXECUTING, SUCCESS} from "../../../core/api/actionTypes";
+import {useNavigation} from "react-navi";
 
 const DeleteFormButton = ({form}) => {
+    const navigation = useNavigation();
     const [open, setOpen] = useState(false);
     const [{status, response}, makeRequest] = useApiRequest(
         `${process.env.REACT_APP_FORMIO_URL}/form/${form._id}`, {verb: 'delete'}
     );
+
+    useEffect(() => {
+        if (status === SUCCESS) {
+            setOpen(false);
+            navigation.navigate("/forms");
+        }
+    }, [status]);
+
     const content = <Container>{status === EXECUTING ?
         <Message icon>
             <Icon name='circle notched' loading/>
@@ -27,7 +37,8 @@ const DeleteFormButton = ({form}) => {
     return <React.Fragment>
         <Button onClick={() => setOpen(true)} negative>Delete</Button>
         <Confirm
-            confirmButton={<Button loading={status === EXECUTING} disabled={status === EXECUTING} negative>Delete</Button>}
+            confirmButton={<Button loading={status === EXECUTING} disabled={status === EXECUTING}
+                                   negative>Delete</Button>}
             open={open}
             header={`Are you sure you wish to delete ${form.name}?`}
             content={content}
