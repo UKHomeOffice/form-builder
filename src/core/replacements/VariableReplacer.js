d78cb7cde8fc77b805f3300326d8e1374f5d1fa0
Amin <amin@digitalpatterns.io>
2019-06-14 07:20:10 +0100

    fixes #11

--- src/core/replacements/VariableReplacer.js
+++ src/core/replacements/VariableReplacer.js
@@ -6,19 +6,19 @@ class VariableReplacer {
         this.replace = this.replace.bind(this);
     }
 
-    replace(data, replacements = null) {
-        let replacedAsJson = dummyjson.parse(JSON.stringify(data));
+    replace(data, replacements) {
+        let asString = JSON.stringify(data);
         if (replacements) {
             const updated = JSON.parse(dummyjson.parse(JSON.stringify(replacements)));
             _.forEach(updated, (replacement) => {
-                const key = Object.keys(replacement);
+                const key = Object.keys(replacement)[0];
                 const value = replacement[key];
                 if (!_.startsWith(value, "{{") && !_.endsWith(value, "}}")) {
-                    replacedAsJson = _.replace(replacedAsJson,key, value);
+                    asString = _.replace(asString,key, value);
                 }
             })
         }
-        return JSON.parse(replacedAsJson);
+        return JSON.parse(asString);
     }
 }
 
