7ec9b5fb8df913056c269e7f1cd83bfc732bc528
aminmc <aminmc@gmail.com>
2019-11-26 15:07:21 +0000

    test signkey

--- client/src/pages/forms/create/components/CreateFormPage.jsx
+++ client/src/pages/forms/create/components/CreateFormPage.jsx
@@ -35,6 +35,7 @@ const CreateFormPage = () => {
         changeJSONEditorMode,
         updateJSON
     } = useCreateForm();
+
     const {envContext} = useEnvContext();
     const {keycloak} = useKeycloak();
 
