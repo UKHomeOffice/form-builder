952c04a6fc7230f854dc2e6bbf81398c5d6757d7
aminmc <aminmc@gmail.com>
2019-12-28 15:14:28 +0000

    updated title

--- client/src/pages/forms/formsRoute.js
+++ client/src/pages/forms/formsRoute.js
@@ -2,12 +2,18 @@ import React from 'react';
 import {lazy, mount, route} from 'navi'
 import FormList from "./list/components/FormList";
 import {withAccessAuthorization, withEnvContext} from "../../core/AppRouter";
+import config from 'react-global-configuration';
+import _ from "lodash";
 
+const environment = (id) => {
+    const environment = _.find(config.get('environments'), {id: id});
+    return environment ? environment.label : id;
+}
 
 export default mount({
-    '/:env': withAccessAuthorization(withEnvContext(route(req =>{
+    '/:env': withAccessAuthorization(withEnvContext(route(req => {
         return {
-            "title": `Forms ${req.params.env}`,
+            "title": `Forms in ${environment(req.params.env)}`,
             "view": <FormList/>
         }
     }))),
