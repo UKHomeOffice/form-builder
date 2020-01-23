5cfbfd45468d5a9e7be6d4d16f6e978c3fdd0cec
Amin <amin@digitalpatterns.io>
2020-01-23 14:24:02 +0000

    updating gds template and return form if failed to interpolate

--- client/src/core/replacements/VariableReplacer.js
+++ client/src/core/replacements/VariableReplacer.js
@@ -12,46 +12,51 @@ class VariableReplacer {
     }
 
     async interpolate(form, environmentContext) {
-        const fakeSubmission = jsf.generate(JSON.parse(JSON.stringify(environmentContext['fakeDataInterpolationContext'])));
+        try {
+            const fakeSubmission = jsf.generate(JSON.parse(JSON.stringify(environmentContext['fakeDataInterpolationContext'])));
 
-        FormioUtils.eachComponent(form.components, component => {
-            component.label = FormioUtils.interpolate(component.label, {
-                data: fakeSubmission.data
+            FormioUtils.eachComponent(form.components, component => {
+                component.label = FormioUtils.interpolate(component.label, {
+                    data: fakeSubmission.data
+                });
+                if (component.type === 'select' && component.data.url !== '') {
+                    component.data.url = FormioUtils.interpolate(component.data.url, {
+                        data: fakeSubmission.data
+                    });
+                }
+                if (component.type === 'content') {
+                    component.html = FormioUtils.interpolate(component.html, {
+                        data: fakeSubmission.data
+                    });
+                }
+                if (component.type === 'htmlelement') {
+                    component.content = FormioUtils.interpolate(component.content, {
+                        data: fakeSubmission.data
+                    });
+                }
+                if (component.defaultValue) {
+                    component.defaultValue = FormioUtils.interpolate(component.defaultValue, {
+                        data: fakeSubmission.data
+                    });
+                }
+                if (component.customDefaultValue && component.customDefaultValue !== '') {
+                    component.defaultValue = FormioUtils.evaluate(component.customDefaultValue, {
+                        data: fakeSubmission.data
+                    }, "value");
+                    component.customDefaultValue = "";
+
+                }
+                if (component.calculateValue && component.calculateValue !== '') {
+                    component.defaultValue = FormioUtils.evaluate(component.calculateValue, {
+                        data: fakeSubmission.data
+                    }, "value");
+                    component.calculateValue = "";
+                }
             });
-            if (component.type === 'select' && component.data.url !== '') {
-                component.data.url = FormioUtils.interpolate(component.data.url, {
-                    data: fakeSubmission.data
-                });
-            }
-            if (component.type === 'content') {
-                component.html = FormioUtils.interpolate(component.html, {
-                    data: fakeSubmission.data
-                });
-            }
-            if (component.type === 'htmlelement') {
-                component.content = FormioUtils.interpolate(component.content, {
-                    data: fakeSubmission.data
-                });
-            }
-            if (component.defaultValue) {
-                component.defaultValue = FormioUtils.interpolate(component.defaultValue, {
-                    data: fakeSubmission.data
-                });
-            }
-            if (component.customDefaultValue && component.customDefaultValue !== '') {
-                component.defaultValue = FormioUtils.evaluate(component.customDefaultValue, {
-                    data: fakeSubmission.data
-                }, "value");
-                component.customDefaultValue = "";
-
-            }
-            if (component.calculateValue && component.calculateValue !== '') {
-                component.defaultValue = FormioUtils.evaluate(component.calculateValue, {
-                    data: fakeSubmission.data
-                }, "value");
-                component.calculateValue = "";
-            }
-        });
+        } catch (e) {
+            console.warn(e);
+            return form;
+        }
         return Promise.resolve(form);
     }
 }
